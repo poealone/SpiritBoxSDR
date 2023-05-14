909dacbd8e876035c804be6d059b445bd5728618
poealone <73013148+poealone@users.noreply.github.com>
2023-05-14 14:36:00 -0400

    switching to pyrtlsdr

--- spiritboxsdr.py
+++ spiritboxsdr.py
@@ -1,51 +1,66 @@
 import numpy as np
-from rtlsdr import RtlSdr
+from pyrtlsdr import RtlSdr
 import sounddevice as sd
+import asyncio
 
-# Get available RTL-SDR devices
-devices = RtlSdr.get_device_info()
+async def get_device_info():
+    sdr = RtlSdr()
+    devices = await sdr.get_device_list()
+    return devices
 
-if len(devices) == 0:
-    print("No RTL-SDR devices found. Please make sure your device is connected.")
-else:
-    # Print device index and name
-    print("Available RTL-SDR devices:")
-    for i, device in enumerate(devices):
-        print(f"Device Index {i}: {device['name']}")
-
-    # Prompt user for RTL-SDR device index
-    device_index = int(input("Enter RTL-SDR device index: "))
-
-    # RTL-SDR settings
-    sdr = RtlSdr(device_index=device_index)
-    sdr.sample_rate = 2.4e6  # Adjust according to your needs
-
-    # Audio settings
-    sample_rate = 44100  # Adjust according to your needs
-    duration = 1.0  # Duration of audio playback in seconds
-
-    # Spirit Box settings
-    carrier_freq = 1000  # Frequency of the carrier signal in Hz
-    modulation_freq = 10  # Frequency of the modulation signal in Hz
-    modulation_amp = 0.5  # Modulation amplitude
-
-    # Generate modulation signal
-    t = np.linspace(0, duration, int(duration * sample_rate), endpoint=False)
-    modulation_signal = modulation_amp * np.sin(2 * np.pi * modulation_freq * t)
-
-    # Main loop
+async def main():
     while True:
-        samples = sdr.read_samples(1024)  # Read samples from RTL-SDR device
+        # Get available RTL-SDR devices
+        devices = await sd.asyncio.run(get_device_info)
 
-        # Demodulate the received samples
-        audio_signal = np.real(samples)  # Use real component as audio signal
+        if len(devices) == 0:
+            print("No RTL-SDR devices found. Please make sure your device is connected.")
+            input("Press Enter to try again...")
+        else:
+            # Print device index and name
+            print("Available RTL-SDR devices:")
+            for i, device in enumerate(devices):
+                print(f"Device Index {i}: {device['name']}")
 
-        # Modulate the audio signal with the carrier signal
-        modulated_audio = np.sin(2 * np.pi * carrier_freq * t + audio_signal)
+            # Prompt user for RTL-SDR device index
+            device_index = int(input("Enter RTL-SDR device index: "))
 
-        # Add modulation to the audio samples
-        output_audio = np.interleave(modulated_audio, modulation_signal)
+            try:
+                # RTL-SDR settings
+                async with RtlSdr(device_index=device_index) as sdr:
+                    # Audio settings
+                    sample_rate = 44100  # Adjust according to your needs
+                    duration = 1.0  # Duration of audio playback in seconds
 
-        # Play the audio
-        sd.play(output_audio, sample_rate)
-        sd.wait()
+                    # Spirit Box settings
+                    carrier_freq = 1000  # Frequency of the carrier signal in Hz
+                    modulation_freq = 10  # Frequency of the modulation signal in Hz
+                    modulation_amp = 0.5  # Modulation amplitude
+
+                    # Generate modulation signal
+                    t = np.linspace(0, duration, int(duration * sample_rate), endpoint=False)
+                    modulation_signal = modulation_amp * np.sin(2 * np.pi * modulation_freq * t)
+
+                    # Main loop
+                    while True:
+                        samples = await sdr.read_samples_async(1024)  # Read samples from RTL-SDR device
+
+                        # Demodulate the received samples
+                        audio_signal = np.real(samples)  # Use real component as audio signal
+
+                        # Modulate the audio signal with the carrier signal
+                        modulated_audio = np.sin(2 * np.pi * carrier_freq * t + audio_signal)
+
+                        # Add modulation to the audio samples
+                        output_audio = np.interleave(modulated_audio, modulation_signal)
+
+                        # Play the audio
+                        sd.play(output_audio, sample_rate)
+                        sd.wait()
+
+            except KeyboardInterrupt:
+                # Stop audio playback and exit on keyboard interrupt
+                sd.stop()
+                break
+
+asyncio.run(main())
